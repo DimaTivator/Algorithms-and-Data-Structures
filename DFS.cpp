2e239690a0f2eb70f149624c3b6b9cb2f10898e2
DimaTivator <82303370+DimaTivator@users.noreply.github.com>
2021-07-09 16:27:18 +0300

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,12 +1,13 @@
 const int N = 10000001;
 bool visited[N];
 vector <int> adj[N];
+
 void dfs(int s) {
     if (visited[s]) {
         return;
-        visited[s] = true;
-        for (auto u : adj[s]) {
-            dfs(u);
-        }
+    }
+    visited[s] = true;
+    for (auto u : adj[s]) {
+        dfs(u);
     }
 }
